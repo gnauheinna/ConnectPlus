ab9020a1e01a62e443b51252bb4089ea9b9c4b62
Kristi Li <kli1226@bu.edu>
2023-10-22 00:09:24 -0400

    Edited codes for styling index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -2,7 +2,7 @@ import { View, Text, Button, TextField } from "react-native-ui-lib";
 import React, { useState } from "react";
 import { StyleSheet, TextInput, Pressable } from "react-native";
 import { Link, useRouter } from "expo-router";
-
+import { TouchableOpacity } from "react-native";
 import { Image } from "expo-image";
 import { LinearGradient } from "expo-linear-gradient";
 import { FontFamily, Color, Border } from "./GlobalStyles";
@@ -20,6 +20,7 @@ import {
   serverTimestamp,
   addDoc,
 } from "firebase/firestore";
+import { CheckBox } from "react-native-elements";
 
 export default function IndexScreen() {
   const [email, setEmail] = useState("");
@@ -106,284 +107,170 @@ export default function IndexScreen() {
   }
 
   return (
-    <LinearGradient
-      style={styles.iphone1415Pro6}
-      locations={[0, 1]}
-      colors={["#fff9e9", "#fff"]}
-    >
-      <Image
-        style={[styles.iphone1415Pro6Child, styles.vectorIcon1Layout]}
-        contentFit="cover"
-        source={require("../assets/images/group-26086143.png")}
-      />
-      <Text style={styles.welcomeToConnect}>{`Welcome to Connect+ `}</Text>
-      <View style={[styles.iphone1415Pro6Item, styles.iphone1415Layout]} />
-      <View style={[styles.iphone1415Pro6Inner, styles.iphone1415Layout]} />
-      <View style={[styles.rectangleView, styles.iphone1415Layout]} />
-      <Image
-        style={[styles.ellipseIcon, styles.ellipseIconLayout]}
-        contentFit="cover"
-        source={require("../assets/images/ellipse-9.png")}
-      />
-      <Image
-        style={[styles.iphone1415Pro6Child1, styles.ellipseIconLayout]}
-        contentFit="cover"
-        source={require("../assets/images/ellipse-10.png")}
-      />
-      <Pressable style={styles.createAccount} onPress={createUser}>
-        <Text>Create Account</Text>
-      </Pressable>
-      <Text style={[styles.email, styles.emailTypo]}>Email</Text>
-      <Text style={[styles.password, styles.emailTypo]}>Password</Text>
-      <Text style={[styles.haveAnAccountContainer, styles.emailTypo]}>
-        <Text style={styles.haveAnAccount}>Have an account?</Text>
-        <Text style={styles.text}>{` `}</Text>
-        <Text style={styles.text}>
-          <Pressable onPress={LogIn}>
-            <Text style={{ textDecorationLine: "underline" }}>Login In</Text>
-          </Pressable>
-        </Text>
-      </Text>
-      <Text style={[styles.rememberMe, styles.emailTypo]}>Remember me</Text>
-      <View style={styles.iphone1415Pro6Child2} />
-      <View style={styles.lineView} />
-      <View style={[styles.iphone1415Pro6Child3, styles.vectorIconLayout]} />
-      <Text style={styles.or}>or</Text>
-      <Image
-        style={[styles.vectorIcon, styles.vectorIconLayout]}
-        contentFit="cover"
-        source={require("../assets/images/vector.png")}
-      />
-      <Pressable onPress={GoogleLogin}>
-        <Image
-          style={[styles.vectorIcon1, styles.vectorIcon1Layout]}
-          source={require("../assets/images/vector1.png")}
-        />
-      </Pressable>
+  <LinearGradient locations={[0, 1]} colors={["#fff9e9", "#fff"]}>
+      <View style={styles.container}>
+      {/* ConnectPlus Logo */}
+      <Image style={[styles.connectPlusLogo]} source={require("../assets/images/connectPlusLogo.png")} />
+
+      {/* Welcome Message */}
+      <Text style={styles.welcomeMessage}>{`Welcome to Connect+ `}</Text>
+
+      {/* Email Input */}
+      <Text style={[styles.inputTitle]}>Email</Text>
       <TextInput
-        style={[styles.examplegmailcom, styles.text1Typo]}
+        style={[styles.input]}
         value={email}
         onChangeText={(email) => setEmail(email)}
-        placeholder="example@gmail.com"
+        // placeholder="example@gmail.com"
       ></TextInput>
+
+      {/* Password Input */}
+      <Text style={[styles.inputTitle]}>Password</Text>
       <TextInput
-        style={[styles.text1, styles.text1Typo]}
+        style={[styles.input]}
         secureTextEntry
         value={password}
         onChangeText={(password) => setPassword(password)}
-        placeholder="123456"
+        // placeholder="123456"
       ></TextInput>
+
+      {/* Checkbox + Remember Me Text */}
+      <View style={styles.rememberMeContainer}>
+        <CheckBox
+            checked={false} // Set the initial checked state here
+            onPress={() => {}} 
+            containerStyle={styles.checkboxContainer}
+        />
+        <Text style={[styles.rememberMeText]}>Remember me</Text>
+      </View>
+
+      {/* Create Account Button */}
+      <TouchableOpacity style={styles.createAccountBtn} onPress={createUser}>
+          <Text style={styles.createAccountText}>Create Account</Text>
+      </TouchableOpacity>
+
+      {/* Divider for 3rd Party Login Options */}
+      <View style={styles.orDivider}>
+          <View style={styles.line}></View>
+          <Text style={{ marginHorizontal: 5}}>OR</Text>
+          <View style={styles.line}></View>
+      </View>
+
+      <View style={[styles.thirdPartyLogIn]}>
+        {/* Google Login Button */}
+        <TouchableOpacity onPress={GoogleLogin}>
+          <Image source={require("../assets/images/googleLogo.png")} style={[styles.thirdPartyIcon]}/>
+        </TouchableOpacity>
+        <View style={{width: 50}}></View>
+        {/* Kerberos Login Button */}
+        <TouchableOpacity>
+          <Image source={require("../assets/images/kerberosLogo.png")} style={[styles.thirdPartyIcon]}/>
+        </TouchableOpacity>
+      </View>
+
+       {/* Switch to Sign In Option */}
+      <View style={[styles.switchToSignIn]}>
+        <Text style={{fontSize: 16}}>Already have an account? </Text>
+        <Text style={{fontSize: 16, fontWeight: "bold"}}>Sign In</Text>
+      </View>
+      </View>
     </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
-  vectorIcon1Layout: {
-    maxWidth: "100%",
-    position: "absolute",
-    maxHeight: "100%",
-    overflow: "hidden",
+  container: {
+    flex: 1,
+    marginLeft: 40,
+    marginRight: 40,
   },
-  iphone1415Layout: {
-    height: 48,
-    position: "absolute",
+  connectPlusLogo:{
+    height: 120, 
+    width: 150,
+    position: "relative",
+    justifyContent: 'center', 
+    alignSelf: 'center',
+    marginTop: 50,
+    marginBottom: 50,
+    resizeMode: "contain", 
   },
-  ellipseIconLayout: {
-    height: 75,
-    width: 75,
-    top: 679,
-    position: "absolute",
-  },
-  emailTypo: {
-    height: 19,
-    fontSize: 16,
-    fontFamily: FontFamily.interRegular,
-    textAlign: "left",
-    position: "absolute",
-  },
-  vectorIconLayout: {
-    width: 40,
-    position: "absolute",
-  },
-  text1Typo: {
-    color: Color.colorGray_200,
-    left: 47,
-    height: 19,
-    width: 190,
-    fontFamily: FontFamily.interRegular,
-    fontSize: 16,
-    textAlign: "left",
-    position: "absolute",
-  },
-  iphone1415Pro6Child: {
-    height: "11.85%",
-    width: "36.68%",
-    top: "8.1%",
-    right: "26.93%",
-    bottom: "80.05%",
-    left: "36.39%",
-    maxHeight: "100%",
-    aspectRatio: 1,
-  },
-  welcomeToConnect: {
-    marginLeft: -160.5,
-    top: 202,
+  welcomeMessage: {
     fontSize: 28,
-    fontWeight: "500",
+    fontWeight: "bold",
     color: "#453b4f",
     textAlign: "center",
-    fontFamily: FontFamily.stolzl,
-    left: "50%",
-    position: "absolute",
+    marginBottom: 40,
   },
-  iphone1415Pro6Item: {
-    top: 316,
-    width: 327,
+  inputTitle: {
+    color: "#000000", 
+    fontSize: 16,
+  },
+  input: {
     borderWidth: 1,
-    borderColor: Color.colorGray_100,
-    borderStyle: "solid",
-    borderRadius: Border.br_8xs,
-    height: 48,
-    left: 36,
-  },
-  iphone1415Pro6Inner: {
-    top: 541,
-    left: 63,
-    borderRadius: 73,
-    backgroundColor: "#e6e6e6",
-    width: 267,
-  },
-  rectangleView: {
-    top: 409,
-    width: 327,
-    borderWidth: 1,
-    borderColor: Color.colorGray_100,
-    borderStyle: "solid",
-    borderRadius: Border.br_8xs,
-    height: 48,
-    left: 36,
-  },
-  ellipseIcon: {
-    marginLeft: -109.5,
-    left: "50%",
-  },
-  iphone1415Pro6Child1: {
-    left: 231,
-  },
-  createAccount: {
-    top: 551,
-    left: 96,
-    color: "#9a969f",
-    textAlign: "left",
-    fontSize: 24,
-    fontFamily: FontFamily.stolzl,
-    position: "absolute",
-  },
-  email: {
-    top: 285,
-    width: 190,
-    height: 19,
-    color: Color.colorBlack,
-    fontSize: 16,
-    left: 36,
-  },
-  password: {
-    top: 382,
-    width: 190,
-    height: 19,
-    color: Color.colorBlack,
-    fontSize: 16,
-    left: 36,
-  },
-  haveAnAccount: {
-    color: Color.colorBlack,
-  },
-  text: {
-    color: Color.colorMediumslateblue,
-  },
-  loginIn1: {
-    textDecorationLine: "underline",
-  },
-  haveAnAccountContainer: {
-    marginLeft: -106.5,
-    top: 809,
-    width: 219,
-    fontSize: 16,
-    height: 19,
-    left: "50%",
-  },
-  rememberMe: {
-    top: 474,
-    left: 70,
-    color: "#9b9b9b",
-    width: 270,
-    fontSize: 16,
-    height: 19,
-  },
-  iphone1415Pro6Child2: {
-    top: 471,
-    borderRadius: 4,
-    backgroundColor: "#d9d9d9",
-    width: 22,
-    height: 22,
-    left: 36,
-    position: "absolute",
-  },
-  lineView: {
-    top: 639,
-    borderColor: Color.colorBlack,
-    borderTopWidth: 1,
-    width: 299,
-    height: 1,
-    left: 47,
-    borderStyle: "solid",
-    position: "absolute",
-  },
-  iphone1415Pro6Child3: {
-    left: 176,
-    backgroundColor: "#fff",
-    height: 40,
-    top: 623,
-  },
-  or: {
-    left: 164,
-    width: 63,
-    top: 623,
-    color: Color.colorBlack,
-    fontFamily: FontFamily.interRegular,
-    fontSize: 24,
-    textAlign: "center",
-    position: "absolute",
-  },
-  vectorIcon: {
-    height: "4.81%",
-    marginLeft: -91.5,
-    top: "81.69%",
-    bottom: "13.5%",
-    left: "50%",
-    maxHeight: "100%",
-  },
-  vectorIcon1: {
-    height: "4.46%",
-    width: "11.96%",
-    top: "81.92%",
-    right: "25.7%",
-    bottom: "13.62%",
-    left: "62.34%",
-    maxHeight: "100%",
-  },
-  examplegmailcom: {
-    top: 332,
-  },
-  text1: {
-    top: 425,
-  },
-  iphone1415Pro6: {
-    borderRadius: 32,
-    flex: 1,
+    borderColor: "#828282",
+    borderRadius: 5,
+    padding: 10,
     width: "100%",
-    height: 852,
-    backgroundColor: "transparent",
-    overflow: "hidden",
+    marginVertical: 10,
+    height: 40,
+  },
+  rememberMeContainer:{
+    flexDirection: "row",
+    justifyContent: "flex-end",
+    alignItems: "center",
+  },
+  rememberMeText: {
+    color: "#9b9b9b",
+    fontSize: 14,
+  },
+  checkboxContainer: {
+    marginRight: 0,
+  },
+  createAccountBtn: {
+    backgroundColor: "#FFC940",
+    marginTop: 40,
+    marginBottom: 40,
+    width: 240,
+    borderRadius: 25,
+    paddingVertical: 12,
+    paddingHorizontal: 20,
+    alignSelf: 'center',
+  },
+  createAccountText: {
+    fontSize: 18,
+    alignSelf: 'center',
+  },
+  orDivider: {
+    marginVertical: 20,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    marginBottom: 30,
+  },
+  line:{
+    height: 1,
+    width: 40,
+    backgroundColor: "#828282",
+  },
+  thirdPartyLogIn:{
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 40,
+  },
+  thirdPartyIcon:{
+    borderRadius: 50,
+    backgroundColor: "#FEF7FF",
+    width: 60,
+    height: 60,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  switchToSignIn:{
+    flexDirection: "row",
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 30,
+    fontSize: 18,
   },
 });
