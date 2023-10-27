dc7c5cb57eb3144c9ab7501d86316b383bf3456d
gnauheinna <116969903+gnauheinna@users.noreply.github.com>
2023-10-27 19:39:13 -0400

    firestore stores new user's userid as document id

--- app/SignUp.tsx
+++ app/SignUp.tsx
@@ -1,20 +1,24 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { View, Text, Button, StyleSheet, Alert } from "react-native";
 import { TextInput } from "react-native-paper";
 import { useRouter } from "expo-router";
 import { TouchableOpacity } from "react-native";
+import firebase from "firebase/app";
 import {
   getAuth,
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
   GoogleAuthProvider,
   signInWithPopup,
+  User,
 } from "firebase/auth";
 import { getApps } from "firebase/app";
 import {
   getFirestore,
-  collection,
+  doc,
   serverTimestamp,
+  setDoc,
+  collection,
   addDoc,
 } from "firebase/firestore";
 import { LinearGradient } from "expo-linear-gradient";
@@ -29,31 +33,66 @@ const SignupForm = () => {
   const [signupError, setSignupError] = useState(null);
   const [major, setMajor] = useState("");
   const [year, setYear] = useState("");
+  const [userID, setUserID] = useState("");
+  const [user, setUser] = useState<User | null>(null);
 
   const router = useRouter();
   const auth = getAuth();
+
+  // create new object
+  const newUser = {
+    email,
+    password,
+    timestamp: serverTimestamp(),
+    name,
+    major,
+    year,
+  };
+
+  //triggers Firebase Auth to create new user
+  function SignUp() {
+    return new Promise<void>((resolve, reject) => {
+      createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+          const user = userCredential.user;
+          setUser(user); // Set the user object
+          console.log("signed up!");
+
+          setSignupError(null);
+          resolve();
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          setSignupError(errorMessage);
+          console.log(errorCode + errorMessage);
+          reject(error);
+        });
+    });
+  }
 
   // saves user data to firestore
   const handleNewUserEmail = async () => {
     // get a instance of Firebase db
     const db = getFirestore();
-    const userCollection = collection(db, "users");
-    // create new object
-    const newUser = {
-      email,
-      password,
-      timestamp: serverTimestamp(),
-      name,
-      major,
-      year,
-    };
+    const user = auth.currentUser;
+    //const userCollection = collection(db, "users");
+    console.log("db");
+    console.log(user?.uid);
+    try {
+      if (user)
+        // await addDoc(userCollection, newUser);
+        await setDoc(doc(db, "users", user.uid), newUser);
+    } catch (error) {
+      console.log(error);
+    }
 
-    await addDoc(userCollection, newUser);
     setEmail("");
     setPassword("");
     setName("");
     setMajor("");
     setYear("");
+    setUserID("");
     // Show the success message
     setShowSuccessMessage(true);
     setTimeout(() => {
@@ -61,88 +100,133 @@ const SignupForm = () => {
     }, 3000);
   };
 
-  //triggers Firebase Auth to create new user
-  function SignUp() {
-    createUserWithEmailAndPassword(auth, email, password)
-      .then((userCredential) => {
-        // Signed up
-        const user = userCredential.user;
-        console.log("signed up!");
-        setSignupError(null);
-      })
-      .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        setSignupError(errorMessage);
-        console.log(errorCode + errorMessage);
-      });
-  }
-
-  const handleSignup = () => {
-    if (password === confirmPassword) {
-      // Passwords match
-      SignUp();
-      handleNewUserEmail();
-      router.push("/interest");
-    } else {
-      // Passwords don't match
-      Alert.alert("Error", "Passwords do not match");
+  const handleSignup = async () => {
+    try {
+      if (password === confirmPassword) {
+        // Passwords match
+        await SignUp();
+        await handleNewUserEmail();
+        router.push("/interest");
+      } else {
+        // Passwords don't match
+        Alert.alert("Error", "Passwords do not match");
+      }
+    } catch (error) {
+      console.log(error);
     }
   };
 
   return (
     <LinearGradient locations={[0, 1]} colors={["#fff9e9", "#fff"]}>
       <View style={styles.container}>
-          <Text style={[styles.title]}>Create Your Account</Text>
-          <Text style={[styles.subTitle]}>Lorem ipsum dolor sit amet consectetur. Quisque mi metus aliquam sed neque.</Text>
+        <Text style={[styles.title]}>Create Your Account</Text>
+        <Text style={[styles.subTitle]}>
+          Lorem ipsum dolor sit amet consectetur. Quisque mi metus aliquam sed
+          neque.
+        </Text>
 
-        <View style={{marginTop: 5}}>
+        <View style={{ marginTop: 5 }}>
           <View style={[styles.inputContainer]}>
-              <Image style={[styles.signUpIcons]} source={require("../assets/images/signUpIcons/name.png")} />
-              <TextInput placeholder="Name" style={[styles.input]} value={name} onChangeText={(name) => setName(name)} placeholderTextColor="#A3A3A3"/>
+            <Image
+              style={[styles.signUpIcons]}
+              source={require("../assets/images/signUpIcons/name.png")}
+            />
+            <TextInput
+              placeholder="Name"
+              style={[styles.input]}
+              value={name}
+              onChangeText={(name) => setName(name)}
+              placeholderTextColor="#A3A3A3"
+            />
           </View>
         </View>
 
-        <View style={{marginTop: 5}}>
+        <View style={{ marginTop: 5 }}>
           <View style={[styles.inputContainer]}>
-              <Image style={[styles.signUpIcons]} source={require("../assets/images/signUpIcons/email.png")} />
-              <TextInput placeholder="Email" style={[styles.input]} value={email} onChangeText={(email) => setEmail(email)} placeholderTextColor="#A3A3A3"/>
+            <Image
+              style={[styles.signUpIcons]}
+              source={require("../assets/images/signUpIcons/email.png")}
+            />
+            <TextInput
+              placeholder="Email"
+              style={[styles.input]}
+              value={email}
+              onChangeText={(email) => setEmail(email)}
+              placeholderTextColor="#A3A3A3"
+            />
           </View>
         </View>
 
-        <View style={{marginTop: 5}}>
+        <View style={{ marginTop: 5 }}>
           <View style={[styles.inputContainer]}>
-              <Image style={[styles.signUpIcons]} source={require("../assets/images/signUpIcons/password.png")} />
-              <TextInput placeholder="Password" style={[styles.input]} value={password} onChangeText={(password) => setPassword(password)} placeholderTextColor="#A3A3A3"/>
+            <Image
+              style={[styles.signUpIcons]}
+              source={require("../assets/images/signUpIcons/password.png")}
+            />
+            <TextInput
+              placeholder="Password"
+              style={[styles.input]}
+              value={password}
+              onChangeText={(password) => setPassword(password)}
+              placeholderTextColor="#A3A3A3"
+            />
           </View>
         </View>
 
-        <View style={{marginTop: 5}}>
+        <View style={{ marginTop: 5 }}>
           <View style={[styles.inputContainer]}>
-              <Image style={[styles.signUpIcons]} source={require("../assets/images/signUpIcons/password.png")} />
-              <TextInput placeholder="Confirm Password" style={[styles.input]} value={confirmPassword} onChangeText={(confirmPassword) => setConfirmPassword(confirmPassword)} placeholderTextColor="#A3A3A3"/>
+            <Image
+              style={[styles.signUpIcons]}
+              source={require("../assets/images/signUpIcons/password.png")}
+            />
+            <TextInput
+              placeholder="Confirm Password"
+              style={[styles.input]}
+              value={confirmPassword}
+              onChangeText={(confirmPassword) =>
+                setConfirmPassword(confirmPassword)
+              }
+              placeholderTextColor="#A3A3A3"
+            />
           </View>
         </View>
 
-        <View style={{marginTop: 5}}>
+        <View style={{ marginTop: 5 }}>
           <View style={[styles.inputContainer]}>
-              <Image style={[styles.signUpIcons]} source={require("../assets/images/signUpIcons/major.png")} />
-              <TextInput placeholder="Major" style={[styles.input]} value={major} onChangeText={(major) => setMajor(major)} placeholderTextColor="#A3A3A3"/>
+            <Image
+              style={[styles.signUpIcons]}
+              source={require("../assets/images/signUpIcons/major.png")}
+            />
+            <TextInput
+              placeholder="Major"
+              style={[styles.input]}
+              value={major}
+              onChangeText={(major) => setMajor(major)}
+              placeholderTextColor="#A3A3A3"
+            />
           </View>
         </View>
 
-        <View style={{marginTop: 5}}>
+        <View style={{ marginTop: 5 }}>
           <View style={[styles.inputContainer]}>
-              <Image style={[styles.signUpIcons]} source={require("../assets/images/signUpIcons/password.png")} />
-              <TextInput placeholder="Year" style={[styles.input]} value={year} onChangeText={(year) => setYear(year)} placeholderTextColor="#A3A3A3"/>
+            <Image
+              style={[styles.signUpIcons]}
+              source={require("../assets/images/signUpIcons/password.png")}
+            />
+            <TextInput
+              placeholder="Year"
+              style={[styles.input]}
+              value={year}
+              onChangeText={(year) => setYear(year)}
+              placeholderTextColor="#A3A3A3"
+            />
           </View>
         </View>
 
-      <TouchableOpacity style={styles.nextButton} onPress={handleSignup}>
+        <TouchableOpacity style={styles.nextButton} onPress={handleSignup}>
           <Text style={styles.nextButtonText}>Next</Text>
-      </TouchableOpacity>
-
-    </View>
+        </TouchableOpacity>
+      </View>
     </LinearGradient>
   );
 };
@@ -154,25 +238,25 @@ const styles = StyleSheet.create({
     padding: 40,
     marginTop: 50,
   },
-  inputContainer:{
+  inputContainer: {
     flexDirection: "row",
   },
-  title:{
+  title: {
     fontSize: 28,
     fontWeight: "bold",
     marginBottom: 20,
     color: "#453B4F",
   },
-  subTitle:{
+  subTitle: {
     fontSize: 16,
     marginBottom: 30,
     color: "#453B4F",
   },
-  signUpIcons:{
+  signUpIcons: {
     width: 26,
     height: 26,
     position: "absolute",
-    resizeMode: "contain", 
+    resizeMode: "contain",
     alignSelf: "center",
   },
   input: {
@@ -194,12 +278,12 @@ const styles = StyleSheet.create({
     borderRadius: 25,
     paddingVertical: 12,
     paddingHorizontal: 20,
-    alignSelf: 'center',
+    alignSelf: "center",
   },
-  nextButtonText:{
+  nextButtonText: {
     fontSize: 18,
-    alignSelf: 'center',
-  }
+    alignSelf: "center",
+  },
 });
 
 export default SignupForm;
--- app/context/UserContext.tsx
+++ app/context/UserContext.tsx
@@ -1,5 +1,92 @@
-interface user {
-  name: string;
-  email: string;
-  password: string;
-}
+// UserContext.js
+import React, {
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+  ReactNode,
+} from "react";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { initializeApp, getApps } from "firebase/app";
+import {
+  getFirestore,
+  collection,
+  getDocs,
+  Timestamp,
+  doc,
+  updateDoc,
+} from "firebase/firestore";
+
+// represents the shape of the context value
+type UserContextType = {
+  user: {
+    name: string;
+    email: string;
+    major: string;
+    year: string;
+  };
+  setUser: (user: {
+    name: string;
+    email: string;
+    major: string;
+    year: string;
+  }) => void;
+};
+
+// createz a new context object and
+const UserContext = createContext<UserContextType>({
+  user: { name: "", email: "", major: "", year: "" },
+  setUser: () => {},
+});
+
+const UserProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
+  const auth = getAuth();
+  const Currentuser = auth.currentUser;
+  const db = getFirestore();
+
+  const [userID, setUserID] = useState<any>();
+
+  const [user, setUser] = useState({
+    name: "",
+    email: "",
+    major: "",
+    year: "",
+  });
+
+  setUserID(Currentuser?.uid);
+  useEffect(() => {
+    // Fetch user data from the "users" collection in Firebase
+    const fetchUser = async () => {
+      try {
+        if (userID) {
+          const usersCollection = collection(db, "users");
+          const userInfo = await getDoc;
+        }
+
+        const userDoc = await db.collection("users").doc("YOUR_USER_ID").get();
+        if (userDoc.exists) {
+          setUser(userDoc.data());
+        } else {
+          setUser({ name: "", email: "", major: "", year: "" });
+        }
+      } catch (error) {
+        console.error("Error fetching user data:", error);
+        setUser({ name: "", email: "", major: "", year: "" });
+      }
+    };
+
+    fetchUser();
+  }, []);
+
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+const useUser = () => {
+  return useContext(UserContext);
+};
+
+export { UserProvider, useUser };
